a25824f7bfcebadd07a96ebe9738237222c1721f
Diwakar <you@example.com>
2024-12-27 12:49:19 +0530

    27-12

---
+++ js/object_method.js
@@ -0,0 +1,24 @@
+var a=[1,3,5,7,9];
+
+document.write(Math.max(a))
+// var res=a.reduce((a,b)=>{
+//     return a+b
+// })
+
+// Remove
+// a.shift() 
+// a.pop()
+
+// Add
+// a.unshift(10)
+// a.push(20)
+
+// console.log(a);
+
+
+// filter
+// find
+// map
+// reduce
+
+// console.log(b);
--- js/table.js
+++ js/table.js
@@ -1,4 +1,6 @@
 let input=document.querySelector("input");
+let div=document.querySelector("div");
+let span=document.querySelector("span");
 async function DefaultFn(){
     // Urll
     const url=await fetch('https://jsonplaceholder.typicode.com/albums')
@@ -6,10 +8,7 @@ async function DefaultFn(){
     const myData=await url.json();
 
     let myKeys=Object.keys(myData[0])
-    // 
-    let div=document.querySelector("div");
-    console.log(input.value);
-    // 
+    
     let table="<table border=1 cellpadding=10><tr>"
     myKeys.map((e)=>{
         table+=`<th>${e}</th>`
@@ -25,5 +24,64 @@ async function DefaultFn(){
     div.innerHTML=table;
 }
 
+async function KeyUp(e){
+    let table="<table border=1 cellpadding=10><tr>";
+    const url=await fetch('https://jsonplaceholder.typicode.com/albums')
+    // Urll
+    const myData=await url.json();
+    const keys=Object.keys(myData[0]);
 
+    keys.map((e)=>{
+        table+=`<th>${e}</th>`
+    })
+    table+="</tr>"
+    const myInput=e.target.value;
+
+    const resultData=myData.filter((e1)=>{
+        return e1.title.startsWith(myInput) ||  e1.id.toString().startsWith(myInput);
+    })
+    span.innerHTML=resultData.length;
+    
+    if(resultData.length>0){
+        resultData.map((e)=>{
+            table+=`<tr>
+                        <td>${e.userId}</td>
+                        <td>${e.id}</td>
+                        <td>${e.title}</td>
+                    </tr>`
+        })
+        div.innerHTML=table;
+    }
+    else{
+        span.innerHTML='';
+        div.innerHTML='No data found'
+    }
+
+    
+}
 window.addEventListener('load',DefaultFn)
+input.addEventListener('keyup',KeyUp)
+
+
+
+
+
+
+
+
+
+// var a=[31,1,4,5,10,3,7,8,9];
+// a.map((e)=>{
+//     console.log(e);
+// })
+
+// const b=a.filter((e)=>{
+//     return e%2!==0;
+// })
+
+// const c=a.find((e)=>{
+//     return e%2!==0;
+// })
+
+// console.log(b);
+// console.log(c);
