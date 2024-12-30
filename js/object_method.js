3a7cd87174886e58c63b852e412c89e8bab3995a
Diwakar <you@example.com>
2024-12-30 13:41:07 +0530

    30-12

--- js/object_method.js
+++ js/object_method.js
@@ -1,6 +1,61 @@
-var a=[1,3,5,7,9];
 
-document.write(Math.max(a))
+
+// document.write(Math.max(a,b,c))
+// document.write("<br>")
+// document.write(Math.min(a,b,c))
+// document.write("<br>")
+// document.write(Math.round(8.93977777))
+// document.write("<br>")
+// document.write(Math.floor(8.93977777))
+// document.write("<br>")
+// var rand=Math.random();
+// var otp=rand*100000
+// document.write(rand)
+// document.write("<br>")
+// document.write(otp)
+// document.write("<br>")
+// document.write(Math.round(otp))
+
+
+
+// let a=new Map([
+//     [10,"Hello"],
+//     [20,"Bye"],
+//     [30,"World"],
+// ])
+// a.set(10,"Hi")
+// console.log(Object.keys(a));
+
+
+
+
+
+
+const fruits = new Map([
+      ["11", 500],
+      ["12", 300],
+      ["13", 200]
+]);
+
+fruits.delete("11");
+// fruits.clear();
+alert(fruits.has('131'))
+console.log(fruits.size);
+
+
+
+// for (const x of fruits.values()){
+//     console.log(x);
+// }
+
+// var a=[10,20,30,40,50];
+
+// for(let x of a){
+//     console.log(x);
+// }
+
+
+// var a1=[10,20,30,40]
 // var res=a.reduce((a,b)=>{
 //     return a+b
 // })
@@ -22,3 +77,7 @@ document.write(Math.max(a))
 // reduce
 
 // console.log(b);
+
+
+
+
