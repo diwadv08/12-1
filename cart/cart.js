25dadec24b3efcb90c5a678b5acd21a572ece2b4
Diwakar <you@example.com>
2025-01-10 13:08:42 +0530

    10-01

--- cart/cart.js
+++ cart/cart.js
@@ -1,34 +1,43 @@
 const cart_row=document.querySelector(".cart_row");
 const cart_count=document.querySelector("#cart_count");
+const modal_div=document.querySelector(".modal-content");
 
-const products=[
+let products=[
     {
-        name:"T-Shirt",
+        name:"Data-1",
         description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, labore voluptas maiores",
         rating:"1.5",
         old_price:"700",
-        new_price:"500",
+        new_price:"300",
+        cart_added:1,
+        img:"0"
     },
     {
-        name:"T-Shirt",
+        name:"Data-2",
         description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, labore voluptas maiores",
         rating:"4.5",
         old_price:"700",
-        new_price:"500",
+        new_price:"600",
+        cart_added:1,
+        img:"1"
     },
     {
-        name:"T-Shirt",
+        name:"Data-3",
         description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, labore voluptas maiores",
         rating:"2.5",
         old_price:"700",
         new_price:"500",
+        cart_added:1,
+        img:"2"
     },
     {
-        name:"T-Shirt",
+        name:"Data-4",
         description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur, labore voluptas maiores",
         rating:"1.5",
         old_price:"700",
-        new_price:"500",
+        new_price:"1300",
+        cart_added:1,
+        img:"3"
     },
     {
         name:"T-Shirt",
@@ -36,6 +45,8 @@ const products=[
         rating:"3.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"4"
     },
     {
         name:"T-Shirt",
@@ -43,6 +54,8 @@ const products=[
         rating:"2.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"5"
     },
     {
         name:"T-Shirt",
@@ -50,6 +63,8 @@ const products=[
         rating:"1.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"6"
     },
     {
         name:"T-Shirt",
@@ -57,6 +72,8 @@ const products=[
         rating:"2.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"7"
     },
     {
         name:"T-Shirt",
@@ -64,6 +81,8 @@ const products=[
         rating:"4.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"8"
     },
     {
         name:"T-Shirt",
@@ -71,6 +90,8 @@ const products=[
         rating:"5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"9"
     },
     {
         name:"T-Shirt",
@@ -78,6 +99,8 @@ const products=[
         rating:"1.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"10"
     },
     {
         name:"T-Shirt",
@@ -85,6 +108,8 @@ const products=[
         rating:"2",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"11"
     },
     {
         name:"T-Shirt",
@@ -92,6 +117,8 @@ const products=[
         rating:"4",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"12"
     },
     {
         name:"T-Shirt",
@@ -99,6 +126,8 @@ const products=[
         rating:"3",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"13"
     },
     {
         name:"T-Shirt",
@@ -106,6 +135,8 @@ const products=[
         rating:"2",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"14"
     },
     {
         name:"T-Shirt",
@@ -113,77 +144,110 @@ const products=[
         rating:"4.5",
         old_price:"700",
         new_price:"500",
+        cart_added:0,
+        img:"15"
     },
     
 ]
-let final_result='';
-let class1='';
-products.map((value,index)=>{
-    if(value.rating>1 && value.rating<2 ){
-        class1='danger'
-    }
-    else if(value.rating>2 && value.rating<4){
-        class1='warning'
-    }
-    else if(value.rating>=4){
-        class1='success'
-    }
-    final_result+=`
-    <div class='col-4 my-3'>
-        <div class='card'>
-            <img src='../images/${index}.jpg' height='200px'>
-            <div class='card-body'>   
-                <h5>${value.name}</h5>
-                <div class='d-flex align-items-center'>
-                    <h6 class='mr-3 text-secondary strike'>Rs.${value.old_price}</h6>
-                    <h5>Rs.${value.new_price}</h5>
+
+    let class1='';
+    let final_result='';
+
+    function producMap(){
+        products.map((value,index)=>{
+            if(value.rating>1 && value.rating<2 ){
+                class1='danger'
+            }
+            else if(value.rating>2 && value.rating<4){
+                class1='warning'
+            }
+            else if(value.rating>=4){
+                class1='success'
+            }
+            let btntext="Add to cart";
+            let btnClass="dark";
+            if(value.cart_added>0){
+                btntext="Already Added";
+                btnClass='secondary disabled';
+            }
+            final_result+=`
+            <div class='col-lg-4 col-md-6 my-3'>
+                <div class='card'>
+                    <img src='../images/${value.img}.jpg' height='200px'>
+                    <div class='card-body'>   
+                        <h5>${value.name}</h5>
+                        <div class='d-flex align-items-center'>
+                            <h6 class='mr-3 text-secondary strike'>Rs.${value.old_price}</h6>
+                            <h5>Rs.${value.new_price}</h5>
+                        </div>
+                        <button class='btn btn-${class1} btn-sm'>${value.rating}</button>
+                        <p>${value.description}</p>
+                        <button class='btn btn-${btnClass} btn-block'>${btntext}</button>
+                    </div>
                 </div>
-                <button class='btn btn-${class1} btn-sm'>${value.rating}</button>
-                <p>${value.description}</p>
-                <button class='btn btn-dark btn-block'>Add to cart</button>
-            </div>
-        </div>
-    </div>`
-    
-})
-const cart_result=[];
-cart_count.innerHTML=cart_result.length;
-
-const modal_div=document.querySelector(".modal-content");
-    if(cart_result.length>0){
-        modal_div.innerHTML=`<table class="table border table-striped">
-                    <tr>
-                        <th>S.No</th>
-                        <th>Name</th>
-                        <th>Image</th>
-                        <th>Price</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Apple</td>
-                        <td><img src="../images/1.jpg" height="40px"></td>
-                        <td>Rs.500</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Orange</td>
-                        <td><img src="../images/2.jpg" height="40px"></td>
-                        <td>Rs.300</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Grapes</td>
-                        <td><img src="../images/3.jpg" height="40px"></td>
-                        <td>Rs.400</td>
-                    </tr>
-                </table>`;
+            </div>`
+            
+        })
     }
-    else{
-        modal_div.innerHTML=`<h1><i class="fa fa-face-sad-tear"></i></h1>
-        <p>No cart Data</p>`;
 
+
+
+    let cart_result=products.filter((e)=>{
+        return e.cart_added>0;
+    });
+
+
+    function DefaultValue(){
+        let myData='',count=1;
+        cart_count.innerHTML=cart_result.length;
+            if(cart_result.length>0){
+                myData=`<table class="table border table-striped">
+                            <tr>
+                                <th>S.No</th>
+                                <th>Name</th>
+                                <th>Image</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>`;
+                cart_result.map((e)=>{
+                    myData+=`<tr>
+                                <td>${count++}</td>
+                                <td>${e.name}</td>
+                                <td><img src="../images/${e.img}.jpg" height="40px"></td>
+                                <td>Rs.${e.new_price}</td>
+                                <td><i class='fa fa-trash' onclick='DeleteFun(${e.img})' data-dismiss='modal'></i></td>
+                            </tr>`
+                })
+                myData+=`</table>`;
+
+                modal_div.innerHTML=myData;
+            }
+            else{
+                modal_div.innerHTML=`<h1><i class="fa fa-face-sad-tear"></i></h1>
+                <p>No cart Data</p>`;
+            }
+        cart_row.innerHTML=final_result;
     }
+
+   
+
+    window.addEventListener('load',producMap())
+
+    function DeleteFun(e){
+        products=products.map((data)=>{
+            if(e==data.img){
+                data.cart_added=0;
+            }
+            return data;
+        })
+
+        console.log(products);
         
+        cart_result=cart_result.filter((product)=>{
+            return e!=product.img;
+        })
+        producMap()
+        DefaultValue()
+    }
 
-
-cart_row.innerHTML=final_result;
+    window.addEventListener('load',DefaultValue())
